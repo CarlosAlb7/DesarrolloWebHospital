e69d31fbd0f41f72f99e74523117f77a8c7fca4e
Danielv210796 <erik.vargas18@tectijuana.edu.mx>
2021-01-11 17:56:31 -0600

    Cambios diseno 11/01/2021

---
+++ PruebaGit/PruebaGit.Web/Migrations/202101112059439_SendEmail.cs
@@ -0,0 +1,16 @@
+namespace PruebaGit.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SendEmail : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
