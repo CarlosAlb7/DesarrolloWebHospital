84eb350ef3fe51373225776dd58dd783cebcb65c
Abraham Rodriguez <71303655+abraham22rodriguez@users.noreply.github.com>
2021-01-13 00:40:10 -0800

    update

--- PruebaGit/PruebaGit.Web/App_Start/RouteConfig.cs
+++ PruebaGit/PruebaGit.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PruebaGit.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Bienvenido", id = UrlParameter.Optional }
             );
         }
     }
